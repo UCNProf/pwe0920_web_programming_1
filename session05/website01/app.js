2e47c5bb7a310e74eab9d04af28186c1fab79000
Christian Wahl <chrwahl@gmail.com>
2020-09-28 13:32:10 +0200

    small changes

--- session05/website01/app.js
+++ session05/website01/app.js
@@ -3,15 +3,30 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mysql = require('mysql');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var blogRouter = require('./routes/blog');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+var db = mysql.createConnection({
+	host: '127.0.0.1',
+	user: 'website01',
+	password: 'eLow8yBSp34wXx',
+	database: 'website01'
+});
+
+db.connect(error => {
+	if(error) throw error;
+	console.log('DB connection done!');
+});
+
+global.db = db;
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +35,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/blog', blogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
